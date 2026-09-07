42311d2b59819a999f54158c086f223a913d10d3
fathulrbntr <fathulrbntr@gmail.com>
2026-09-08 01:38:19 +0700

    add google maps location

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const branches = {
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.64899299478!2d107.17485607570174!3d-6.309759893679511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699b004552c909%3A0xd66bfe9f65d9050f!2sManiacduren_cikarang!5e0!3m2!1sid!2sid!4v1788804084667!5m2!1sid!2sid",
 
     link:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.64899299478!2d107.17485607570174!3d-6.309759893679511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699b004552c909%3A0xd66bfe9f65d9050f!2sManiacduren_cikarang!5e0!3m2!1sid!2sid!4v1788804084667!5m2!1sid!2sid"
+        "https://maps.app.goo.gl/g43DRz5TfeLvbmvRA"
   },
 
 
@@ -33,7 +33,7 @@ const branches = {
     map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.4465132177124!2d107.14625387570193!3d-6.33616179365353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699b77cf6eb591%3A0x38797d3c3e8beebd!2sManiac%20Duren%20Lippo%20CIkarang!5e0!3m2!1sid!2sid!4v1788805757174!5m2!1sid!2sid",
     link:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.4465132177124!2d107.14625387570193!3d-6.33616179365353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699b77cf6eb591%3A0x38797d3c3e8beebd!2sManiac%20Duren%20Lippo%20CIkarang!5e0!3m2!1sid!2sid!4v1788805757174!5m2!1sid!2sid"
+        "https://maps.app.goo.gl/yGsXWvqrb73hcGEt7"
   },
 
 
@@ -47,7 +47,7 @@ const branches = {
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.718277440453!2d106.97462667570083!3d-6.168466693818844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698b23f292285d%3A0xe12431e0ac1c573b!2sManiac%20duren%20nusantara!5e0!3m2!1sid!2sid!4v1788805889670!5m2!1sid!2sid",
 
     link:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.718277440453!2d106.97462667570083!3d-6.168466693818844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698b23f292285d%3A0xe12431e0ac1c573b!2sManiac%20duren%20nusantara!5e0!3m2!1sid!2sid!4v1788805889670!5m2!1sid!2sidI"
+        "https://maps.app.goo.gl/kuHqcwivMLR8pVXz8"
   }
 
 };
