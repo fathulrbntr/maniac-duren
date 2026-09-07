e9fa32867bcb2f143ceb4d4acef679ba4f1db839
fathulrbntr <fathulrbntr@gmail.com>
2026-09-08 01:42:37 +0700

    fix product section layout

--- script.js
+++ script.js
@@ -48,7 +48,21 @@ const branches = {
 
     link:
         "https://maps.app.goo.gl/kuHqcwivMLR8pVXz8"
-  }
+  },
+
+    neo: {
+    name: "Depot Durian Neo Patio",
+
+    address:
+      "Cikarang, Kabupaten Bekasi, Jawa Barat",
+
+    map:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.3801705476203!2d107.14861859999999!3d-6.344788599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699bd5094cc5e7%3A0xe35180e85a9c3f3c!2sDepot%20Durian!5e0!3m2!1sid!2sid!4v1788806485087!5m2!1sid!2sid",
+
+    link:
+        "https://maps.app.goo.gl/uZhvVnJdB4PUPuLSA"
+  },
+
 
 };
 
