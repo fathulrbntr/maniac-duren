b5bc9f178af9d7f1b329eadeac54d1f4b1d4a084
fathulrbntr <fathulrbntr@gmail.com>
2026-09-08 01:22:55 +0700

    add google maps location

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log("Maniac Duren website aktif");
 
 const branches = {
 
-  cikarang: {
+  jababeka: {
     name: "Maniac Duren Cikarang",
 
     address:
@@ -24,15 +24,14 @@ const branches = {
   },
 
 
-  depok: {
+  lippo: {
     name: "Maniac Duren Depok",
 
     address:
       "Masukkan alamat cabang Depok",
 
     map:
-      "MASUKKAN_EMBED_MAP_DEPOK",
-
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.64899299478!2d107.17485607570174!3d-6.309759893679511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699b004552c909%3A0xd66bfe9f65d9050f!2sManiacduren_cikarang!5e0!3m2!1sid!2sid!4v1788804084667!5m2!1sid!2sid",
     link:
       "MASUKKAN_LINK_MAP_DEPOK"
   },
